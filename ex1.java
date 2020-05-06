c38f291aa0127cb8d32133491860313110a93f80
diogoBenevenuto <63623546+diogoBenevenuto@users.noreply.github.com>
2020-05-05 21:17:43 -0300

    ex 1 dos 10

---
+++ ex1.java
@@ -0,0 +1,28 @@
+package exercico_condiconal;
+
+import java.util.Scanner;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		
+			int num;
+			
+			Scanner leitor = new Scanner (System.in);
+			
+				System.out.println("Digite um numero: ");
+			num = leitor.nextInt();
+			
+			if (num > 20) 
+				System.out.println(num);
+			
+			
+			
+			}
+			
+
+}
+
+
